9a9826fec744a755f1697eaae1add05b967a2a06
emmeiwhite <emmeiwhite@gmail.com>
2025-06-19 14:41:38 +0530

    fetch-async-data 04- setup queryClient

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ThemeProvider from './context/ThemeContext.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Step-1: CREATE A QueryClient instance
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </StrictMode>
+  </QueryClientProvider>
 )
