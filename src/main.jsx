ed2fd7bd5757b73b27ecffcae62c882e775643c4
emmeiwhite <emmeiwhite@gmail.com>
2025-06-19 14:44:44 +0530

    fetch-async-data 05- staleTime setup globally, this is the time until which we'll get cached data

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,13 @@ import ThemeProvider from './context/ThemeContext.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Step-1: CREATE A QueryClient instance
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5 // 5 minutes 1000ms = 1s, 1s*60=1min, 1min*5 = 5 min
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
